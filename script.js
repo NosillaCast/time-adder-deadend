f8424209af22c160c4a8368a92d4eec8632d658d
Allison Sheridan <allison@podfeet.com>
2021-08-30 21:08:39 -0700

    Feat: validation from PBS66
    thought this was already saved

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ let timeInputArray = [
     sec: "sec-2"
   }
 ]
+
+// Document ready handler
+$(function(){
+  // get a reference to the form for quick access
+  const $form = $('#support_fm');
+
+  // add event handlers to activate the Bootstrap validation styles
+  // when the form is first interacted with
+  const enableBootstrapValiationStyles = function(){
+    $form.addClass('was-validated');
+  };
+  $form.submit(enableBootstrapValiationStyles);
+  $('input', $form).on('invalid', enableBootstrapValiationStyles);
+
+  // add a submit handler to the form
+  $form.submit(function(){
+  window.alert("Your donation was not submitted because this is a dummy form, but thanks all the same, it's the thought that counts :)");
+  });
+}); // End document ready handler
