4106bd7ae0c7599a1c71e32360d682f116c1a65c
Allison Sheridan <allison@podfeet.com>
2021-08-31 11:25:37 -0700

    Feat: working validation

--- script.js
+++ script.js
@@ -16,15 +16,48 @@ let timeInputArray = [
 // Document ready handler
 $(function(){
   // get a reference to the form for quick access
-  const $form = $('#support_fm');
+  const $form = $('#form-1');
+  const $hours  = $('#hours-1');
+  const $minutes = $('#min-1');
+  const $seconds = $('#sec-1');
 
   // add event handlers to activate the Bootstrap validation styles
-  // when the form is first interacted with
-  const enableBootstrapValiationStyles = function(){
-    $form.addClass('was-validated');
-  };
-  $form.submit(enableBootstrapValiationStyles);
-  $('input', $form).on('invalid', enableBootstrapValiationStyles);
+ 
+  const validateTimes = function(){
+    if($hours.is(':valid')){
+      $hours.removeClass('is-invalid').addClass('is-valid');
+      console.log(`I validated hours as valid`);
+    }else{
+      $hours.removeClass('is-valid').addClass('is-invalid');
+      console.log(`I validated hours as invalid`);
+    }
+    if($minutes.is(':valid')){
+      $minutes.removeClass('is-invalid').addClass('is-valid');
+      console.log(`I validated min`);
+    }else{
+      $minutes.removeClass('is-valid').addClass('is-invalid');
+    }
+    if($seconds.is(':valid')){
+      $seconds.removeClass('is-invalid').addClass('is-valid');
+      console.log(`I validated sec`);
+    }else{
+      $seconds.removeClass('is-valid').addClass('is-invalid');
+    }
+    if ($hours.is(':valid') && $minutes.is(':valid')){
+      $('#hr-min-feedback').hide();
+      return true;
+    }
+    if ($sec.is(':valid')){
+      $('#sec-feedback').hide();
+      return true;
+    }
+  }
+    
+  // add input handler to the times
+  // BUG: the input is triggering before I type
+  $hours.on('input',console.log(`poop`));
+  $minutes.on('input',validateTimes);
+  $seconds.on('input',validateTimes);
 
   // add a submit handler to the form
   $form.submit(function(){
