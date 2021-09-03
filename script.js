7f2a395be2655589eadbe65811bacffc2a01fdd3
Allison Sheridan <allison@podfeet.com>
2021-09-02 20:59:06 -0700

    Trying to create loop through rows to make an array
    failing miserably, all features currently broken - validation & adding to array

--- script.js
+++ script.js
@@ -1,76 +1,78 @@
 
+  
+  
 
-let timeInputArray = [
-  {
-    hours: "hours-1",
-    min: "min-1",
-    sec: "sec-1"
-  },
-  {
-    hours: "hours-2",
-    min: "min-2",
-    sec: "sec-2"
-  }
-]
 
 // Document ready handler
 $(function(){
+
+
+  let timeInputArray = new Array();
+  
   // get a reference to the form for quick access
-  const $form = $('#form-1');
-  const $hours  = $('#hours-1');
-  const $min = $('#min-1');
-  const $sec = $('#sec-1');
-  const $btn1 = $('#addBtn-1');
-  const $btn2 = $('#subBtn-1');
+  for (i=1; i<=2; i++) {
+    const $form = $('#form-i');
+    const $hours = $('#hours-i');
+    const $min = $('#min-i');
+    const $sec = $('#sec-i');
+    const $btnA = $('#addBtn-i');
+    const $btnS = $('#subBtn-i');
 
-  // add event handlers to activate the Bootstrap validation styles
- 
-  const validateHrsMin = function(){
-    if($hours.is(':valid')){
-      $hours.removeClass('is-invalid').addClass('is-valid');
-    }else{
-      $hours.removeClass('is-valid').addClass('is-invalid');
+    // add event handlers to activate the Bootstrap validation styles
+    const validateHrsMin = function(){
+      if($hours.is(':valid')){
+        $hours.removeClass('is-invalid').addClass('is-valid');
+        window.alert("Hours is valid");
+      }else{
+        $hours.removeClass('is-valid').addClass('is-invalid');
+      }
+      if($min.is(':valid')){
+        $min.removeClass('is-invalid').addClass('is-valid');
+      }else{
+        $min.removeClass('is-valid').addClass('is-invalid');
+      }
+      if ($hours.is(':valid') && $min.is(':valid')){
+        $('#hr-min-feedback-i').hide();
+        return true;
+      }
+      $('#hr-min-feedback-i').show();
+      return false;
     }
-    if($min.is(':valid')){
-      $min.removeClass('is-invalid').addClass('is-valid');
-    }else{
-      $min.removeClass('is-valid').addClass('is-invalid');
+    const validateSec = function(){
+      if($sec.is(':valid')){
+        $sec.removeClass('is-invalid').addClass('is-valid');
+      }else{
+        $sec.removeClass('is-valid').addClass('is-invalid');
+      }
+      if ($sec.is(':valid')){
+        $('#sec-feedback-i').hide();
+        return true;
+      }
+      $('#sec-feedback-i').show();
+      return false;
     }
-    if ($hours.is(':valid') && $min.is(':valid')){
-      $('#hr-min-feedback-1').hide();
-      return true;
-    }
-    $('#hr-min-feedback-1').show();
-    return false;
-  }
-  const validateSec = function(){
-    if($sec.is(':valid')){
-      $sec.removeClass('is-invalid').addClass('is-valid');
-    }else{
-      $sec.removeClass('is-valid').addClass('is-invalid');
-    }
-    if ($sec.is(':valid')){
-      $('#sec-feedback-1').hide();
-      return true;
-    }
-    $('#sec-feedback-1').show();
-    return false;
-  }
-    
-  // add input handler to the times
-  $hours.on('input', validateHrsMin);
-  $min.on('input',validateHrsMin);
-  $sec.on('input',validateSec);
+      
+    // add input handler to the times to run validation
+    $hours.on('input', validateHrsMin);
+    $min.on('input',validateHrsMin);
+    $sec.on('input',validateSec);
 
-  // click handlers for add/subtract/moreTime buttons
-  $btn1.click(function(){
-    window.alert(`You clicked add`);
-  });
-  $btn2.click(function(){
-    window.alert(`You clicked subtract`);
-  });
-  $('#moreTimes').click(function(){
-    window.alert(`You asked for more times`)
-  })
+    // click handlers for add/subtract/moreTime buttons
+    $btnA.click(function(){
+      window.alert($hours.val())
+      timeInputArray[0] = new Array ($hours.val(), $min.val(), $sec.val());
+      console.log(timeInputArray);
+
+
+
+    });
+    $btnS.click(function(){
+      window.alert(`You clicked subtract`);
+    });
+    $('#moreTimes').click(function(){
+      window.alert(`You asked for more times`)
+    })
+
+  } // end of for loop
 
 }); // End document ready handler
